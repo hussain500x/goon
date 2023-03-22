4a10100b6a835abdbec05d61e0efa2106f2fe27a
Hussain Almarzooq <hhaalmarzouq@gmail.com>
2023-03-23 00:12:05 +0300

    test

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,58 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Color.fromRGBO(215, 238, 234, 0.31),
         appBar: AppBar(
-          title: Text('hussain'),
+          title: Text('test'),
           backgroundColor: Color.fromRGBO(63, 151, 153, 1),
         ),
-        body: Center(
-          child: Image(
-            image: AssetImage('assets\images\chat-gpt-logo.jpg'),
-          ),
-        ),
+        body: SafeArea(
+            child: GridView.count(
+          primary: false,
+          padding: const EdgeInsets.all(20),
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+          crossAxisCount: 2,
+          children: <Widget>[
+            CircleAvatar(
+              radius: 1,
+              backgroundImage: AssetImage('images/110.png'),
+            ),
+            CircleAvatar(
+              radius: 1,
+              backgroundImage: AssetImage('images/111.png'),
+            ),
+            CircleAvatar(
+              radius: 1,
+              backgroundImage: AssetImage('images/116.png'),
+            ),
+            CircleAvatar(
+              radius: 1,
+              backgroundImage: AssetImage('images/117.png'),
+            ),
+            CircleAvatar(
+              radius: 1,
+              backgroundImage: AssetImage('images/118.png'),
+            ),
+            CircleAvatar(
+              radius: 1,
+              backgroundImage: AssetImage('images/123.png'),
+            ),
+            CircleAvatar(
+              radius: 1,
+              backgroundImage: AssetImage('images/110.png'),
+            ),
+          ],
+        )),
       ),
-    ),
-  );
+    );
+  }
 }
